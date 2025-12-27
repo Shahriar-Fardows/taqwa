1d527b5c4c0adfb181b8ad4e229acfa2d8020faa
Shahriar Fardows <shahriarfardows@gmail.com>
2025-12-28 02:05:47 +0600

    about page done

--- src/components/home/EventSection.tsx
+++ src/components/home/EventSection.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 import Image from "next/image"
 import Link from "next/link"
